576e5391258238fb4114d6d8ccd93bb4cbd1b604
jaywomack <womackjw@hotmail.com>
2021-01-03 19:07:19 -0800

    deleted and starting from scratch with new design

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,5 @@
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <h1>The Knowledge of Man Blog</h1>
-      <div className='links'>
-        <a href='/'>Home</a>
-        <a href='/create'>New Blog</a>
-      </div>
-    </nav>
-  );
+  return <nav className='navbar'></nav>;
 };
 
 export default Navbar;
