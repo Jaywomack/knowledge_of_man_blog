76c654c73a4c712e9a23b800771ea7f96428adba
jaywomack <womackjw@hotmail.com>
2021-01-14 21:30:19 -0800

    Creating styles for papers

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -13,6 +13,16 @@ const useStyles = makeStyles((theme) => ({
       height: theme.spacing(16),
     },
   },
+  papers: {
+    backgroundColor: 'orange',
+    color: 'white',
+    height: '15vh',
+    width: '15vw',
+    display: 'flex',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
 
 export default function CardsContainer() {
@@ -20,14 +30,14 @@ export default function CardsContainer() {
 
   return (
     <div className={classes.root}>
-      <Paper elevation={3}>
-        <Typography>I'm in the Paper Component</Typography>
+      <Paper className={classes.papers} elevation={6}>
+        <Typography>Health</Typography>
       </Paper>
-      <Paper elevation={3}>
-        <Typography>I'm in the Paper Component</Typography>
+      <Paper className={classes.papers} elevation={6}>
+        <Typography>Wealth</Typography>
       </Paper>
-      <Paper elevation={3}>
-        <Typography>I'm in the Paper Component</Typography>
+      <Paper className={classes.papers} elevation={6}>
+        <Typography>Wisdom</Typography>
       </Paper>
     </div>
   );
